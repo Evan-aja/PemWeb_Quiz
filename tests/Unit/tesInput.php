46002e6a2aafc54d29f7ae605f382f6f8a0342e3
Evan-aja <gebel909@student.ub.ac.id>
2022-05-26 14:12:06 +0700

    Date fixes

--- tests/Unit/tesInput.php
+++ tests/Unit/tesInput.php
@@ -33,6 +33,7 @@ class tesInput extends TestCase
             "tanggal"=>"2009-05-26"
         ]);
         $response->assertStatus(200);
+        $response->assertViewIs('result');
     }
     public function test_result(): void
     {
@@ -46,7 +47,7 @@ class tesInput extends TestCase
         $this->assertSame("Sabtu Wage 28 Mei 2022",$data['hasil']['tiga']);
         $this->assertSame("Rabu Pon 01 Juni 2022",$data['hasil']['tujuh']);
         $this->assertSame("Selasa Pahing 05 Juli 2022",$data['hasil']['empatpuluh']);
-        $this->assertSame("Senin Wage 05 September 2022",$data['hasil']['seratus']);
+        $this->assertSame("Minggu Pon 04 September 2022",$data['hasil']['seratus']);
         $this->assertSame("Jumat Pahing 26 Mei 2023",$data['hasil']['pendhakpisan']);
         $this->assertSame("Minggu Pon 26 Mei 2024",$data['hasil']['pendhakkalih']);
         $this->assertSame("Sabtu Pon 26 April 2025",$data['hasil']['seribu']);
